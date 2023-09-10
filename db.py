4a7758e2dd973c0ff8129303d2e49df0e1a95636
jferlo <jferlopezt@gmail.com>
2023-09-10 01:54:46 -0500

    version con carga en db

---
+++ db.py
@@ -0,0 +1,32 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class MongoDriver:
+    def __init__(self):
+        user = os.getenv('MONGO_USER')
+        password = os.getenv('MONGO_PASSWORD')
+        hostname = os.getenv('MONGO_HOSTNAME')
+
+        uri = f"mongodb+srv://{user}:{password}@{hostname}/?retryWrites=true&w=majority"
+
+
+        # Create a new client and connect to the server
+        self.client = MongoClient(uri, server_api=ServerApi('1'))
+        # Send a ping to confirm a successful connection
+
+    def insert_record(self, record: dict, username: str):
+        self.client.get_database('db_prueba_final').get_collection(f'{username}_inmuebles').insert_one(record)
+
+    def test_connection(self):
+        try:
+            self.client.admin.command('ping')
+            print("Pinged your deployment. You successfully connected to MongoDB!")
+        except Exception as e:
+            print(e)
+
+
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-#from db import MongoDriver
+from db import MongoDriver
 
 driver = webdriver.Chrome()
 driver.get("https://www.remax.com.ec/")
 search_box = driver.find_element(by=By.CSS_SELECTOR, value="#searchbar-input")
 search_box.send_keys("Pinar Alto")
+
 search_button = driver.find_element(by=By.CSS_SELECTOR, value="#button-search")
 search_button.click()
 
 house_cards = driver.find_elements(By.CSS_SELECTOR,"#wrapper")
+mongodb = MongoDriver()
 
 for card in house_cards:
     titulo= card.find_element(By.CSS_SELECTOR,"div.mat-ripple.info > div > h2").text
@@ -19,13 +21,11 @@ for card in house_cards:
     print(f'$ {precio}')
     print(features)
     print("----------------------------------------------------")
-    grabar= {
+    inmueble_anuncio= {
         'titulo':titulo,
         'precio':precio,
         'features':features
     }
-    db.
-
-
+    mongodb.insert_record(record=inmueble_anuncio, username="Juan Fernando")
 
 driver.close()
